45b4512d37e2cd724b5e94f868eb719f3c0a4a48
AgentAbi <135492667+AgentAbi@users.noreply.github.com>
2024-03-16 11:20:36 +0530

    Update p8.c

--- c/p8.c
+++ c/p8.c
@@ -2,10 +2,10 @@
 
 #include <stdio.h>
 
-float computeAverage(float* x, size_t* N) {
+float computeAverage(float* x, size_t N) {
     float numerator = 0.0;
     float denominator = 0.0;
-    for (size_t i = 0; i < (size_t)N; i++) {
+    for (size_t i = 0; i < N; i++) {
         numerator += x[i];
         denominator++;
     }
@@ -16,6 +16,6 @@ int main()
 {
     float x[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     size_t N = 10;
-    printf("%.3g\n", computeAverage(x, &N));
+    printf("%.3g\n", computeAverage(x, N));
     return 0;
 }
