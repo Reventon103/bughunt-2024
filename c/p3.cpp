266743f6da2a6990ce2b3522b51f8c6b516c9179
AgentAbi <135492667+AgentAbi@users.noreply.github.com>
2024-03-16 12:07:38 +0530

    Update p3.cpp

--- c/p3.cpp
+++ c/p3.cpp
@@ -5,26 +5,35 @@
 #include<vector>
 using namespace std;
 
+#include<iostream>
+#include<vector>
+using namespace std;
+
 vector<int> productExceptSelf(vector<int>& nums) {
-    vector<int> ans(nums.size());
-    ans[0] = 1;
-    for(int i=1;i<nums.size();i++){
-        ans[i] = ans[i-1] * nums[i-1];
+    vector<int> ans(nums.size(), 1);
+    int left = 1, right = 1;
+    
+    for(int i = 0; i < nums.size(); i++) {
+        ans[i] *= left;
+        left *= nums[i];
     }
-    int r = 1;
-    for(int i=nums.size()-1;i>=0;i--){
-        ans[i] *= r;
-        r = nums[i]*r;
+    
+    for(int i = nums.size() - 1; i >= 0; i--) {
+        ans[i] *= right;
+        right *= nums[i];
     }
+    
     return ans;
 }
 
-int main(){
-    vector<int> nums = {1,2,3,4};
+int main() {
+    vector<int> nums = {1, 2, 3, 4};
     vector<int> ans = productExceptSelf(nums);
-    for(int i=0;i<ans.size();i++){
-        cout<<ans[i]<<" ";
+    
+    for(int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
+    
     return 0;
 }
